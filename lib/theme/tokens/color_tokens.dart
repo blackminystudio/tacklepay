198559e778df488b09a9f9c1823b7554b8841f88
Krushnakanta Nayak <letswork.aneek@gmail.com>
2024-12-05 14:02:17 +0530

    color setup

--- lib/theme/tokens/color_tokens.dart
+++ lib/theme/tokens/color_tokens.dart
@@ -2,11 +2,14 @@
 import 'package:flutter/material.dart';
 
 class ColorTokens {
-  static const Color primary = Color(0xFF6200EE);
-  static const Color onPrimary = Color(0xFFFFFFFF);
-  static const Color secondary = Color(0xFF03DAC6);
-  static const Color onSecondary = Color(0xFF000000);
-  static const Color background = Color(0xFFF5F5F5);
-  static const Color surface = Color(0xFFFFFFFF);
-  static const Color error = Color(0xFFB00020);
+  static const Color primary = Color(0xFF00CD7E);
+  static const Color primaryDark = Color(0xFF008653);
+  static const Color secondary = Color(0xFFFA5A2A);
+  static const Color secondaryDark = Color(0xFFDE3300);
+  static const Color contrastDark = Color(0xFF535B66);
+  static const Color contrastMedium = Color(0xFF86898D);
+  static const Color contrastLow = Color(0xFFE0E0E0);
+  static const Color contrastLight = Color(0xFFF4F4F4);
+  static const Color dark = Color(0xFF000000);
+  static const Color light = Color(0xFFFFFFFF);
 }
