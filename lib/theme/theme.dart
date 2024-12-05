470da75a19b76b2a14e9973a2e0194e5d44e7fa3
Krushnakanta Nayak <letswork.aneek@gmail.com>
2024-12-05 16:32:22 +0530

    spacing setup

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,13 +2,16 @@
 
 import 'package:flutter/material.dart';
 import 'extensions/miny_colors.dart';
+import 'extensions/miny_spacing.dart';
 
 final ThemeData appTheme = ThemeData(
-  extensions: <ThemeExtension<dynamic>>[
-    const MinyColors(),
+  extensions: const <ThemeExtension<dynamic>>[
+    MinyColors(),
+    MinySpacing(),
   ],
 );
 
 extension ThemeExtensions on ThemeData {
   MinyColors get colors => extension<MinyColors>() ?? const MinyColors();
+  MinySpacing get spacing => extension<MinySpacing>() ?? const MinySpacing();
 }
