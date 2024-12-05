12a246b25a3e03b615a485559b8ef2ac181942e0
Krushnakanta Nayak <letswork.aneek@gmail.com>
2024-12-06 02:50:00 +0530

    elevation setup

---
+++ lib/theme/tokens/elevation_tokens.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ElevationTokens {
+  static const BoxShadow e1 = BoxShadow(
+    color: Color(0x3E000000),
+    offset: Offset(0.0, 1.0),
+    blurRadius: 10.0,
+  );
+}
