8fe2b41c2a2e5242506358d8641c00be5f77971e
Krushnakanta Nayak <letswork.aneek@gmail.com>
2024-12-09 01:55:09 +0530

    upi card created

---
+++ lib/upi_card.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'theme/theme.dart';
+
+class UPICard extends StatelessWidget {
+  final String payeeFirstName;
+  final String payeeLasttName;
+  final String payeeUpiAdress;
+  const UPICard({
+    super.key,
+    required this.payeeFirstName,
+    required this.payeeLasttName,
+    required this.payeeUpiAdress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final firstNameInitial = payeeFirstName[0];
+    final lastNameInitial = payeeLasttName[0];
+
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(theme.spacing.width.s12),
+      decoration: BoxDecoration(
+        color: theme.colors.contrastLight,
+        borderRadius: BorderRadius.circular(
+          theme.borderradius.medium,
+        ),
+        border: Border.all(
+          color: theme.colors.contrastLow,
+        ),
+      ),
+      child: Row(
+        children: [
+          // headingTitle Container
+          buildIcon(theme, firstNameInitial, lastNameInitial),
+          SizedBox(width: theme.spacing.width.s12),
+          buildContent(theme),
+        ],
+      ),
+    );
+  }
+
+  Column buildContent(ThemeData theme) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            '$payeeFirstName $payeeLasttName',
+            style: theme.textStyle.headingSmallRegular
+                .copyWith(color: theme.colors.contrastDark),
+          ),
+          Text(
+            payeeUpiAdress,
+            style: theme.textStyle.bodyRegular
+                .copyWith(color: theme.colors.contrastMedium),
+          ),
+        ],
+      );
+
+  Container buildIcon(
+          ThemeData theme, String firstNameInitial, String lastNameInitial) =>
+      Container(
+        height: theme.sizing.width.s12,
+        width: theme.sizing.width.s12,
+        decoration: BoxDecoration(
+          color: theme.colors.light,
+          borderRadius: BorderRadius.circular(
+            theme.borderradius.full(theme.sizing.width.s12),
+          ),
+        ),
+        child: Center(
+// Name Initial
+          child: Text(
+            '$firstNameInitial$lastNameInitial',
+            style: theme.textStyle.headingSmallMedium.copyWith(
+              color: theme.colors.contrastDark,
+            ),
+          ),
+        ),
+      );
+}
