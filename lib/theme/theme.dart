cd45e8ca05455fab85868f166b5b5b0b79688a98
akira <akira2001official@gmail.com>
2024-12-09 15:19:06 +0530

    Created icon tokens

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -8,6 +8,8 @@ import 'extensions/miny_sizing.dart';
 import 'extensions/miny_spacing.dart';
 import 'extensions/miny_typography.dart';
 
+export 'miny_icons.dart';
+
 final ThemeData appTheme = ThemeData(
   extensions: <ThemeExtension<dynamic>>[
     // These are used with non-const as they are providing dynamic sizes
@@ -33,15 +35,11 @@ extension ThemeExtensions on ThemeData {
   // accroding to the screen sizes
   MinySizing get sizing => extension<MinySizing>() ?? MinySizing();
   MinySpacing get spacing => extension<MinySpacing>() ?? MinySpacing();
-  MinyTypography get textStyle =>
-      extension<MinyTypography>() ?? MinyTypography();
+  MinyTypography get textStyle => extension<MinyTypography>() ?? MinyTypography();
   // These are fix sizes accroding to the design system
   MinyColors get colors => extension<MinyColors>() ?? const MinyColors();
   MinyOpacity get opacity => extension<MinyOpacity>() ?? const MinyOpacity();
-  MinyElevation get elevation =>
-      extension<MinyElevation>() ?? const MinyElevation();
-  MinyBorderWidth get borderwidth =>
-      extension<MinyBorderWidth>() ?? const MinyBorderWidth();
-  MinyBorderRadius get borderradius =>
-      extension<MinyBorderRadius>() ?? const MinyBorderRadius();
+  MinyElevation get elevation => extension<MinyElevation>() ?? const MinyElevation();
+  MinyBorderWidth get borderwidth => extension<MinyBorderWidth>() ?? const MinyBorderWidth();
+  MinyBorderRadius get borderradius => extension<MinyBorderRadius>() ?? const MinyBorderRadius();
 }
