074d6325c460130f26035b367a1329f0e2683660
Nilesh  Kumar Singh <nileshkumarsingh9245@gmail.com>
2024-12-10 00:43:30 +0530

    UpiInfoCard: Removed the Card Widget.

--- lib/widgets/upi_info_card.dart
+++ lib/widgets/upi_info_card.dart
@@ -24,84 +24,78 @@ class _UpiInfoCardState extends State<UpiInfoCard> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Card(
-      color: theme.colors.contrastLight,
-      shape: RoundedRectangleBorder(
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: theme.colors.contrastLow,
+          width: theme.borderwidth.small,
+        ),
         borderRadius: BorderRadius.circular(theme.borderradius.medium),
       ),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: theme.colors.contrastLow,
-            width: theme.borderwidth.small,
-          ),
-          borderRadius: BorderRadius.circular(theme.borderradius.medium),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(theme.spacing.width.s16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    textAmount,
-                    style: theme.textStyle.bodyBold.copyWith(
-                      color: theme.colors.contrastMedium,
-                    ),
+      child: Padding(
+        padding: EdgeInsets.all(theme.spacing.width.s16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  textAmount,
+                  style: theme.textStyle.bodyBold.copyWith(
+                    color: theme.colors.contrastMedium,
                   ),
-                  SizedBox(
-                    width: theme.sizing.width.s32,
-                    child: TextField(
-                      controller: _amountController,
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) => setState(() {}),
-                      decoration: InputDecoration(
-                        hintText: hintTextAmount,
-                        border: InputBorder.none,
-                        hintStyle: theme.textStyle.bodyBold
-                            .copyWith(color: theme.colors.contrastLow),
-                      ),
-                      style: theme.textStyle.titleBold
-                          .copyWith(color: theme.colors.contrastDark),
-                      textAlign: TextAlign.right,
+                ),
+                SizedBox(
+                  width: theme.sizing.width.s32,
+                  child: TextField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) => setState(() {}),
+                    decoration: InputDecoration(
+                      hintText: hintTextAmount,
+                      border: InputBorder.none,
+                      hintStyle: theme.textStyle.bodyBold
+                          .copyWith(color: theme.colors.contrastLow),
                     ),
+                    style: theme.textStyle.titleBold
+                        .copyWith(color: theme.colors.contrastDark),
+                    textAlign: TextAlign.right,
                   ),
-                ],
-              ),
-              SizedBox(height: theme.spacing.width.s12),
-              const Divider(),
-              SizedBox(height: theme.spacing.width.s12),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    textMessage,
-                    style: theme.textStyle.bodyBold.copyWith(
-                      color: theme.colors.contrastMedium,
+                ),
+              ],
+            ),
+            SizedBox(height: theme.spacing.width.s12),
+            const Divider(),
+            SizedBox(height: theme.spacing.width.s12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  textMessage,
+                  style: theme.textStyle.bodyBold.copyWith(
+                    color: theme.colors.contrastMedium,
+                  ),
+                ),
+                Flexible(
+                  child: TextField(
+                    controller: _messageController,
+                    onChanged: (value) => setState(() {}),
+                    decoration: InputDecoration(
+                      hintText: hintTextMessage,
+                      border: InputBorder.none,
+                      hintStyle: theme.textStyle.bodyBold
+                          .copyWith(color: theme.colors.contrastLow),
                     ),
+                    style: theme.textStyle.headingSmallMedium
+                        .copyWith(color: theme.colors.contrastMedium),
+                    textAlign: TextAlign.right,
                   ),
-                  Flexible(
-                    child: TextField(
-                      controller: _messageController,
-                      onChanged: (value) => setState(() {}),
-                      decoration: InputDecoration(
-                        hintText: hintTextMessage,
-                        border: InputBorder.none,
-                        hintStyle: theme.textStyle.bodyBold
-                            .copyWith(color: theme.colors.contrastLow),
-                      ),
-                      style: theme.textStyle.headingSmallMedium
-                          .copyWith(color: theme.colors.contrastMedium),
-                      textAlign: TextAlign.right,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
