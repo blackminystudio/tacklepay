aff4a43e6dc6134f2160f6ad74ef3ee59f2029c8
akira <akira2001official@gmail.com>
2024-12-09 15:27:26 +0530

    format issues resolved

--- lib/theme/miny_icons.dart
+++ lib/theme/miny_icons.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
 class MinyIcons {
-  static const IconData outline_arrow_up = Iconsax.arrow_up_2;
+  static const IconData outlineArrowUp = Iconsax.arrow_up_2;
   static const IconData plus = Icons.add_rounded;
-  static const IconData fill_scan = Iconsax.scan5;
-  static const IconData outline_send_money = Iconsax.card_send;
-  static const IconData outline_receive_money = Iconsax.card_receive;
+  static const IconData fillScan = Iconsax.scan5;
+  static const IconData outlineSendMoney = Iconsax.card_send;
+  static const IconData outlineReceiveMoney = Iconsax.card_receive;
   static const IconData cross = Icons.close_rounded;
   static const IconData filter = Iconsax.setting_4;
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -35,11 +35,15 @@ extension ThemeExtensions on ThemeData {
   // accroding to the screen sizes
   MinySizing get sizing => extension<MinySizing>() ?? MinySizing();
   MinySpacing get spacing => extension<MinySpacing>() ?? MinySpacing();
-  MinyTypography get textStyle => extension<MinyTypography>() ?? MinyTypography();
+  MinyTypography get textStyle =>
+      extension<MinyTypography>() ?? MinyTypography();
   // These are fix sizes accroding to the design system
   MinyColors get colors => extension<MinyColors>() ?? const MinyColors();
   MinyOpacity get opacity => extension<MinyOpacity>() ?? const MinyOpacity();
-  MinyElevation get elevation => extension<MinyElevation>() ?? const MinyElevation();
-  MinyBorderWidth get borderwidth => extension<MinyBorderWidth>() ?? const MinyBorderWidth();
-  MinyBorderRadius get borderradius => extension<MinyBorderRadius>() ?? const MinyBorderRadius();
+  MinyElevation get elevation =>
+      extension<MinyElevation>() ?? const MinyElevation();
+  MinyBorderWidth get borderwidth =>
+      extension<MinyBorderWidth>() ?? const MinyBorderWidth();
+  MinyBorderRadius get borderradius =>
+      extension<MinyBorderRadius>() ?? const MinyBorderRadius();
 }
