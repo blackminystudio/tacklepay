17b6dbe38c93e3a88b0b91a471a583b8939d00f9
Nilesh  Kumar Singh <nileshkumarsingh9245@gmail.com>
2024-12-14 15:12:14 +0530

    Updated main.dart

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'theme/theme.dart';
+import 'widgets/string_constants.dart';
+
+void main() {
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(440, 956), // Base design size (width x height)
+      minTextAdapt: true,
+      builder: (context, _) => const TacklePay(),
+    ),
+  );
+}
+
+class TacklePay extends StatelessWidget {
+  const TacklePay({super.key});
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: appName,
+        theme: appTheme,
+        home: HomeScreen(),
+      );
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      backgroundColor: theme.colors.secondary,
+      body: Container(),
+    );
+  }
+}
