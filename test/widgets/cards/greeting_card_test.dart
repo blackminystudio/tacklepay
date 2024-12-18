16f121b51868bf19685bd8823afa973813d0e17b
Krushnakanta Nayak <letswork.aneek@gmail.com>
2024-12-18 22:33:17 +0530

    update user name

--- test/widgets/cards/greeting_card_test.dart
+++ test/widgets/cards/greeting_card_test.dart
@@ -7,7 +7,7 @@ import 'package:tackleapp/widgets/cards/greeting_card.dart';
 void main() {
   group('GreetingCard Widget Tests', () {
     const testGreetingMessage = 'Hello, Welcome!';
-    const testUserName = 'John Doe';
+    const testUserName = 'John';
     const testProfilePictureUrl = 'https://example.com/profile-picture.jpg';
 
     Widget createWidgetUnderTest({
@@ -30,56 +30,33 @@ void main() {
         );
 
     testWidgets(
-        'Given valid greeting message, user name, and profile picture URL '
-        'when GreetingCard is rendered '
-        'Then it should display the greetingmessage, username, profilepicture',
-        (WidgetTester tester) async {
-      // Act
-      await mockNetworkImagesFor(
-        () async {
-          await tester.pumpWidget(
-            createWidgetUnderTest(
-              greetingMessage: testGreetingMessage,
-              userName: testUserName,
-              profilePictureUrl: testProfilePictureUrl,
-            ),
-          );
-        },
-      );
+      'Given valid greeting message, user name, and profile picture URL '
+      'when GreetingCard is rendered '
+      'Then it should display the greeting message, username, profile picture',
+      (WidgetTester tester) async {
+        // Act
+        await mockNetworkImagesFor(
+          () async {
+            await tester.pumpWidget(
+              createWidgetUnderTest(
+                greetingMessage: testGreetingMessage,
+                userName: testUserName,
+                profilePictureUrl: testProfilePictureUrl,
+              ),
+            );
+          },
+        );
 
-      // Assert
-      expect(find.text(testGreetingMessage), findsOneWidget);
-      expect(find.text(testUserName), findsOneWidget);
-      expect(find.byType(CircleAvatar), findsOneWidget);
-    });
+        // Assert
+        expect(find.text(testGreetingMessage), findsOneWidget);
+        expect(find.text(testUserName), findsOneWidget);
+        expect(find.byType(CircleAvatar), findsOneWidget);
 
-    testWidgets(
-        'Given a long user name '
-        'when GreetingCard is rendered '
-        'Then it should truncate the user name with ellipsis',
-        (WidgetTester tester) async {
-      // Arrange
-      const longUserName = 'Johnathan Alexander Maximillian the Fourth';
-
-      // Act
-
-      await mockNetworkImagesFor(
-        () async {
-          await tester.pumpWidget(
-            createWidgetUnderTest(
-              userName: longUserName,
-              greetingMessage: testGreetingMessage,
-              profilePictureUrl: testProfilePictureUrl,
-            ),
-          );
-        },
-      );
-      // Assert
-      final userNameFinder = find.text(longUserName);
-      final userNameWidget = tester.widget<Text>(userNameFinder);
-      expect(userNameWidget.maxLines, 1);
-      expect(userNameWidget.overflow, TextOverflow.ellipsis);
-    });
+        final avatarWidget =
+            tester.widget<CircleAvatar>(find.byType(CircleAvatar));
+        expect(avatarWidget.backgroundImage, isNotNull);
+      },
+    );
 
     testWidgets(
         'Given an empty profile picture URL '
