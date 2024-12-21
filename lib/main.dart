c2c05e676efce087820a11ae3e336db71dffa3d6
Satyabrata Nayak <satyabratanayakofficial@gmail.com>
2024-12-21 17:49:45 +0530

    Firebase Emulator setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,9 @@
 import 'dart:async';
-import 'dart:io';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
-import 'flavors.dart';
+import 'core/config/firebase_options/firebase_config.dart';
 
 Future<void> main() async {
-  await createDummyData(Flavors.name, Platform.isAndroid ? 'Android' : 'iOS');
+  await FirebaseService.initialize();
   runApp(const App());
-}
-
-Future<void> createDummyData(String flavor, String platform) async {
-  try {
-    final firestore = FirebaseFirestore.instance;
-    final data = {'message': 'Welcome to $flavor on $platform'};
-    await firestore.collection('TacklePayDev').add(data);
-    print('Dummy data created successfully!');
-  } catch (e) {
-    print('Failed to create dummy data: $e');
-  }
 }
--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,16 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../flavors.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text(Flavors.title),
-        ),
+        appBar: AppBar(title: Text(Flavors.title)),
         body: Center(
-          child: Text(
-            'Hello ${Flavors.title}',
+          child: Column(
+            children: [
+              Text('Hello ${Flavors.title}'),
+              ElevatedButton(
+                onPressed: () async {
+                  await testWriteData();
+                },
+                child: const Text('add Data'),
+              )
+            ],
           ),
         ),
       );
+}
+
+Future<void> testWriteData() async {
+  await FirebaseFirestore.instance.collection('users').add({
+    'name': 'Test User',
+    'createdAt': DateTime.now(),
+  });
 }
