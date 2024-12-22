c41950146392860789d4c0edca95f6b302fe4202
Satyabrata Nayak <satyabratanayakofficial@gmail.com>
2024-12-22 17:20:21 +0530

    firebase setup

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+
+import 'firebase_options_prod.dart';
 import 'flavors.dart';
 
 import 'main.dart' as runner;
 
 Future<void> main() async {
   F.appFlavor = Flavor.prod;
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: ProdFirebaseOptions.currentPlatform,
+  );
   await runner.main();
 }
