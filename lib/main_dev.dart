9c8ffb4db04e9c2a19f8a5a10b0a470ceb3b45f0
Satyabrata Nayak <satyabratanayakofficial@gmail.com>
2024-12-22 17:20:21 +0530

    firebase setup

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,8 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+
+import 'firebase_options_dev.dart';
 import 'flavors.dart';
 
 import 'main.dart' as runner;
 
 Future<void> main() async {
   F.appFlavor = Flavor.dev;
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DevFirebaseOptions.currentPlatform,
+  );
+  final data = {'msg': 'Hello'};
+  await FirebaseFirestore.instance.collection('Test').add(data);
   await runner.main();
 }
