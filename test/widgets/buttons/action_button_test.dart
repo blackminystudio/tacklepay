58b23bed9e7f60d9761a003743dba6e04f9dbc8e
akira <akira2001official@gmail.com>
2024-12-24 20:21:58 +0530

    updated test cases

--- test/widgets/buttons/action_button_test.dart
+++ test/widgets/buttons/action_button_test.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tackleapp/theme/theme.dart';
 import 'package:tackleapp/widgets/buttons/action_button.dart';
 
 void main() {
-  const testIcon = Icons.add;
+  const testIcon = MinyIcons.plus;
   const testTitle = 'Test Button';
   group('ActionButton Widget Tests', () {
     Widget createWidgetUnderTest({
@@ -31,78 +32,161 @@ void main() {
     testWidgets(
         'Given title and icon '
         'when ActionButton is rendered '
-        'Then it should display both title and icon',
-        (WidgetTester tester) async {
-      // Arrange
-
-      await tester.pumpWidget(
-        createWidgetUnderTest(
-          title: testTitle,
-          icon: testIcon,
-        ),
-      );
-
-      // Assert
-      expect(find.text(testTitle), findsOneWidget);
-      expect(find.byIcon(testIcon), findsOneWidget);
-    });
-
-    testWidgets(
-        'Given only title '
-        'when ActionButton is rendered '
-        'Then it should display only the title', (WidgetTester tester) async {
-      // Arrange
-
-      await tester.pumpWidget(
-        createWidgetUnderTest(
-          title: testTitle,
-        ),
-      );
-
-      // Assert
-      expect(find.text(testTitle), findsOneWidget);
-      expect(find.byType(Icon), findsNothing);
-    });
-
-    testWidgets(
-        'Given only icon '
-        'when ActionButton is rendered '
-        'Then it should display only the icon', (WidgetTester tester) async {
-      // Arrange
-
-      await tester.pumpWidget(
-        createWidgetUnderTest(
-          icon: testIcon,
-        ),
-      );
-
-      // Assert
-      expect(find.byIcon(testIcon), findsOneWidget);
-      expect(find.byType(Text), findsNothing);
-    });
-
-    testWidgets(
-        'Given ActionButton is rendered '
-        'When title and icon are provided '
-        'Then it should display both title and icon',
-        (WidgetTester tester) async {
+        'Then it displays both title and icon', (WidgetTester tester) async {
       // Arrange
       var isTapped = false;
-
       await tester.pumpWidget(
         createWidgetUnderTest(
           title: testTitle,
-          onTap: () {
-            isTapped = true;
-          },
+          icon: testIcon,
+          onTap: () => isTapped = true,
         ),
       );
 
       // Act
       await tester.tap(find.text(testTitle));
       await tester.pump();
+
       // Assert
+      expect(find.text(testTitle), findsOneWidget);
+      expect(find.byIcon(testIcon), findsOneWidget);
       expect(isTapped, isTrue);
+    });
+
+    testWidgets(
+        'Given only title '
+        'when ActionButton is rendered '
+        'Then it displays only the title ', (WidgetTester tester) async {
+      // Arrange
+      var isTapped = false;
+      await tester.pumpWidget(
+        createWidgetUnderTest(
+          title: testTitle,
+          onTap: () => isTapped = true,
+        ),
+      );
+
+      // Act
+      await tester.tap(find.text(testTitle));
+      await tester.pump();
+
+      // Assert
+      expect(find.text(testTitle), findsOneWidget);
+      expect(find.byIcon(testIcon), findsNothing);
+      expect(isTapped, isTrue);
+    });
+
+    testWidgets(
+        'Given only icon '
+        'when ActionButton is rendered '
+        'Then it displays only the icon', (WidgetTester tester) async {
+      // Arrange
+      var isTapped = false;
+      await tester.pumpWidget(
+        createWidgetUnderTest(
+          icon: testIcon,
+          onTap: () => isTapped = true,
+        ),
+      );
+
+      // Act
+      await tester.tap(find.byIcon(testIcon));
+      await tester.pump();
+
+      // Assert
+      expect(find.byIcon(testIcon), findsOneWidget);
+      expect(find.text(testTitle), findsNothing);
+      expect(isTapped, isTrue);
+    });
+
+    testWidgets(
+        'Given no icon and no title '
+        'when ActionButton is rendered '
+        'Then it displays blank and is clickable ',
+        (WidgetTester tester) async {
+      // Arrange
+      var isTapped = false;
+      await tester.pumpWidget(
+        createWidgetUnderTest(
+          onTap: () => isTapped = true,
+        ),
+      );
+
+      // Act
+      final buttonFinder = find.byType(ActionButton);
+      await tester.tap(buttonFinder);
+      await tester.pump();
+
+      // Assert
+      expect(find.text(testTitle), findsNothing);
+      expect(find.byIcon(testIcon), findsNothing);
+      expect(buttonFinder, findsOneWidget);
+      expect(isTapped, isTrue);
+    });
+
+    testWidgets(
+        'Given fillScan icon '
+        'when ActionButton is rendered '
+        'Then it applies the correct offset', (WidgetTester tester) async {
+      // Arrange
+      const fillScanIcon = MinyIcons.fillScan;
+      await tester.pumpWidget(
+        createWidgetUnderTest(
+          icon: fillScanIcon,
+        ),
+      );
+
+      // Assert
+      expect(find.byIcon(fillScanIcon), findsOneWidget);
+
+      // Verify the offset is applied via Transform widget
+      final transformFinder = find.ancestor(
+        of: find.byIcon(fillScanIcon),
+        matching: find.byType(Transform),
+      );
+
+      // Ensure the Transform widget exists
+      expect(transformFinder, findsOneWidget);
+
+      // Extract the Transform widget and check its offset
+      final transform = tester.widget<Transform>(transformFinder);
+      final translation = transform.transform.getTranslation();
+      expect(translation.x, isNot(0));
+      expect(translation.x, lessThan(0));
+      expect(translation.y, equals(0));
+    });
+
+    testWidgets(
+        'Given fillScan icon and title '
+        'when ActionButton is rendered '
+        'Then it applies the correct offset', (WidgetTester tester) async {
+      // Arrange
+      const fillScanIcon = MinyIcons.fillScan;
+      await tester.pumpWidget(
+        createWidgetUnderTest(
+          icon: fillScanIcon,
+          title: testTitle,
+        ),
+      );
+
+      // Assert
+      expect(find.byIcon(fillScanIcon), findsOneWidget);
+
+      // Verify the offset is applied via Transform widget
+      final transformFinder = find.ancestor(
+        of: find.byIcon(fillScanIcon),
+        matching: find.byType(Transform),
+      );
+
+      // Ensure the Transform widget exists
+      expect(transformFinder, findsOneWidget);
+
+      // Extract the Transform widget and check its offset
+      final transform = tester.widget<Transform>(transformFinder);
+      final translation = transform.transform.getTranslation();
+      expect(translation.x, isNot(0));
+      expect(translation.x, lessThan(0));
+      expect(translation.y, equals(0));
     });
   });
 }
