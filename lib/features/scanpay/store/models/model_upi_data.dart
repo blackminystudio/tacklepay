3fc89adc785827db5ad94255fe43f35a6ad91ca9
Satyabrata Nayak <satyabratanayakofficial@gmail.com>
2024-12-29 18:07:23 +0530

    added model and make tags dynamic

---
+++ lib/features/scanpay/store/models/model_upi_data.dart
@@ -0,0 +1,11 @@
+class UpiDataModel {
+  final String payeeFirstName;
+  final String payeeLastName;
+  final String payeeUpiId;
+
+  UpiDataModel({
+    required this.payeeFirstName,
+    required this.payeeLastName,
+    required this.payeeUpiId,
+  });
+}
--- lib/features/scanpay/ui/widgets/bottomsheets/bottomsheet_scan_n_pay.dart
+++ lib/features/scanpay/ui/widgets/bottomsheets/bottomsheet_scan_n_pay.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 import '../../../../../theme/theme.dart';
@@ -7,98 +9,173 @@ import '../../../../../widgets/buttons/pay_using_button.dart';
 import '../../../../../widgets/cards/tag_card.dart';
 import '../../../../../widgets/cards/upi_card.dart';
 import '../../../../../widgets/cards/upi_info_card.dart';
+import '../../../store/models/model_payusing.dart';
+import '../../../store/models/model_upi_data.dart';
+
+///
+/// Output:
+/// UPISchema
+/// Amount
+/// Message
+/// TagList
+
+List<PayUsingModel> getPayUsing() {
+  final listOfPayUsing = <PayUsingModel>[
+    PayUsingModel('imageUrl', 'PhonePe', 'upiPrefix'),
+    PayUsingModel('imageUrl', 'Paytm', 'upiPrefix'),
+    PayUsingModel('imageUrl', 'Amazon Pay', 'upiPrefix'),
+    PayUsingModel('imageUrl', 'Google Pay', 'upiPrefix'),
+  ];
+  return listOfPayUsing;
+}
+
+// TODO: UPI URL
+String getUPIUrl({
+  required PayUsingModel selectedPayusingModel,
+}) =>
+    'upi://';
+
+UpiDataModel getUpiData() {
+  final upiDataModel = UpiDataModel(
+    payeeFirstName: 'Maa',
+    payeeLastName: 'Tarini Center',
+    payeeUpiId: 'maatarinicenter1332@ybl',
+  );
+  return upiDataModel;
+}
+
+Future<void> saveData({
+  required String amount,
+  required String message,
+  required List<String> tags,
+}) async {
+  final dateTime = DateTime.now();
+  // Call Store Save Method
+  log('AllData : $amount, $message, $tags, $dateTime');
+}
 
 Future showScanNPayBottomSheet(BuildContext context) async {
   final theme = Theme.of(context);
+
+  // Getting data from the services
+  final listOfPayUsing = getPayUsing();
+  final upiDatamodel = getUpiData();
+
+  // Initialisation of local Data
+  final tagList = <String>[];
+  const _amount = '';
+  const _message = '';
+  final _selectedPayusingModel = listOfPayUsing[0];
+
+  final upiUrl = getUPIUrl(selectedPayusingModel: _selectedPayusingModel);
+
   await showScaffoldBottomsheet(
     context,
-    // actionButton: _buildTextActionButton(context),
     children: [
-      _buildScanPayNowBody(theme),
-      _buildPayNowButtonBody(),
+      _buildScanPayNowBody(
+        theme: theme,
+        tagList: tagList,
+        upiDataModel: upiDatamodel,
+      ),
+      _buildPayNowButtonBody(
+        listOfPayUsing,
+        upiUrl,
+        _amount,
+        _message,
+        tagList,
+      ),
     ],
   );
 }
 
-Row _buildPayNowButtonBody() => Row(
+Row _buildPayNowButtonBody(
+  List<PayUsingModel> listOfPayUsing,
+  String upiUrl,
+  String _amount,
+  String _message,
+  List<String> tagList,
+) =>
+    Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         Expanded(
           child: PayUsingButton(listOfPayUsing: listOfPayUsing),
         ),
-        const ActionButton(
+        ActionButton(
           title: 'Pay Now',
+          onTap: () {
+            log('UPIURL: $upiUrl');
+            saveData(
+              amount: _amount,
+              message: _message,
+              tags: tagList,
+            );
+          },
         )
       ],
     );
 
-StatefulBuilder _buildScanPayNowBody(ThemeData theme) {
-  final tagList = <String>['Groc', 'Home2'];
-  return StatefulBuilder(builder: (context, setState) {
-    final scrollController = ScrollController();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      scrollController.animateTo(
-        scrollController.position.maxScrollExtent,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeOut,
-      );
-    });
-    return Expanded(
-      child: SingleChildScrollView(
-        controller: scrollController,
-        child: Column(
-          children: [
-            SizedBox(height: theme.sizing.height.s5),
-            const UPICard(
-              payeeFirstName: 'Maa',
-              payeeLastName: 'Tarini Center',
-              payeeUpiId: 'maatarinicenter1332@ybl',
-            ),
-            SizedBox(height: theme.sizing.height.s6),
-            const UpiInfoCard(),
-            SizedBox(height: theme.sizing.height.s6),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Wrap(
-                spacing: theme.spacing.width.s12,
-                runSpacing: theme.spacing.width.s12,
-                children: [
-                  ...tagList.map(
-                    (e) => TagCard(
-                      tagType: TagType.info,
-                      text: e,
-                      onDelete: () {
+StatefulBuilder _buildScanPayNowBody({
+  required ThemeData theme,
+  required List<String> tagList,
+  required UpiDataModel upiDataModel,
+}) =>
+    StatefulBuilder(builder: (context, setState) {
+      final scrollController = ScrollController();
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        scrollController.animateTo(
+          scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      });
+      return Expanded(
+        child: SingleChildScrollView(
+          controller: scrollController,
+          child: Column(
+            children: [
+              SizedBox(height: theme.sizing.height.s5),
+              UPICard(
+                payeeFirstName: upiDataModel.payeeFirstName,
+                payeeLastName: upiDataModel.payeeLastName,
+                payeeUpiId: upiDataModel.payeeUpiId,
+              ),
+              SizedBox(height: theme.sizing.height.s6),
+              const UpiInfoCard(),
+              SizedBox(height: theme.sizing.height.s6),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Wrap(
+                  spacing: theme.spacing.width.s12,
+                  runSpacing: theme.spacing.width.s12,
+                  children: [
+                    ...tagList.map(
+                      (e) => TagCard(
+                        tagType: TagType.info,
+                        text: e,
+                        onDelete: () {
+                          setState(() {
+                            tagList.remove(e);
+                          });
+                        },
+                      ),
+                    ),
+                    TagCard(
+                      tagType: TagType.create,
+                      onTextSubmit: (value) {
                         setState(() {
-                          tagList.remove(e);
+                          tagList.add(value);
                         });
                       },
                     ),
-                  ),
-                  TagCard(
-                    tagType: TagType.create,
-                    onTextSubmit: (value) {
-                      setState(() {
-                        tagList.add(value);
-                      });
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).viewInsets.bottom,
-            ),
-          ],
+              const SizedBox(height: 20),
+              SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
+            ],
+          ),
         ),
-      ),
-    );
-  });
-}
-
-List<PayUsingModel> listOfPayUsing = [
-  PayUsingModel('imageUrl', 'providerName', 'upiPrefix'),
-  PayUsingModel('imageUrl', 'providerName', 'upiPrefix'),
-  PayUsingModel('imageUrl', 'providerName', 'upiPrefix'),
-  PayUsingModel('imageUrl', 'providerName', 'upiPrefix'),
-];
+      );
+    });
