140b6d341938bb95e13437dd8c8f9a236d0bf6ac
Satyabrata Nayak <69607211+satyabratanayak@users.noreply.github.com>
2024-12-30 21:49:55 +0530

    Features/bottomsheet filters (#58)
    * init
    
    * Slider update
    
    * Completed Filter page
    
    * Testcase edited
    
    ---------
    
    Co-authored-by: Krushnakanta Nayak <letswork.aneek@gmail.com>

--- lib/features/history/ui/widgets/bottomSheets/bottomsheet_filters.dart
+++ lib/features/history/ui/widgets/bottomSheets/bottomsheet_filters.dart
@@ -1,1 +1,55 @@
+import 'package:flutter/material.dart';
+import '../../../../../widgets/bottomSheets/bottomsheet_scaffold.dart';
+import '../../pages/filters/filter_page.dart';
 
+// Data From Database
+List<String> yearList = [
+  '2023',
+  '2024',
+];
+List<String> monthList = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+Future showFiltersButtomSheet(BuildContext context) async {
+  await showScaffoldBottomsheet(
+    title: 'All Filters',
+    context,
+    // actionButton: _buildTextActionButton(context),
+    children: [
+      const FilterView(),
+    ],
+  );
+}
+
+// GestureDetector _buildTextActionButton(
+//   BuildContext context,
+// ) {
+//   final theme = Theme.of(context);
+//   return GestureDetector(
+//     onTap: () => Navigator.pop(context),
+//     child: Container(
+//       color: theme.colors.transparent,
+//       padding: EdgeInsets.only(
+//         top: theme.spacing.width.s4,
+//         left: theme.spacing.width.s4,
+//         bottom: theme.spacing.width.s4,
+//       ),
+//       child: Text(
+//         'Reset',
+//         style: theme.textStyle.bodyRegular,
+//       ),
+//     ),
+//   );
+// }
