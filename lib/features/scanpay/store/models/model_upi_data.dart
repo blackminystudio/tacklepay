781f9bb6d2ea23fff0feac8ed66edec0867a3ad9
Satyabrata Nayak <satyabratanayakofficial@gmail.com>
2024-12-31 15:10:36 +0530

    scan page build

--- lib/features/scanpay/store/models/model_upi_data.dart
+++ lib/features/scanpay/store/models/model_upi_data.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 class UpiDataModel {
   final String payeeFirstName;
   final String payeeLastName;
@@ -8,4 +10,29 @@ class UpiDataModel {
     required this.payeeLastName,
     required this.payeeUpiId,
   });
+  static UpiDataModel? parseUpiString(String upiString) {
+    try {
+      final uri = Uri.parse(upiString);
+
+      // Extract the UPI parameters
+      final payeeName = uri.queryParameters['pn'] ?? '';
+      final payeeUpiId = uri.queryParameters['pa'] ?? '';
+
+      // Split the payee's full name into first and last names
+      final nameParts = payeeName.split(' ');
+      final payeeFirstName = nameParts.isNotEmpty ? nameParts.first : '';
+      final payeeLastName =
+          nameParts.length > 1 ? nameParts.sublist(1).join(' ') : '';
+
+      // Create and return the UpiDataModel
+      return UpiDataModel(
+        payeeFirstName: payeeFirstName,
+        payeeLastName: payeeLastName,
+        payeeUpiId: payeeUpiId,
+      );
+    } catch (e) {
+      log('Error parsing UPI string: $e');
+      return null; // Return null in case of failure
+    }
+  }
 }
--- lib/features/scanpay/ui/widgets/bottomsheets/bottomsheet_scan_n_pay.dart
+++ lib/features/scanpay/ui/widgets/bottomsheets/bottomsheet_scan_n_pay.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../../widgets/bottomsheet/bottomsheet_scaffold.dart';
+import '../../../store/models/model_upi_data.dart';
 import '../../../store/scanpay_store.dart';
 import '../../pages/scan_pay_page.dart';
 
-Future showScanNPayBottomSheet(BuildContext context) async {
+Future showScanNPayBottomSheet(
+  BuildContext context,
+  UpiDataModel upiData,
+) async {
   final theme = Theme.of(context);
   final store = Provider.of<ScanpayStore>(context, listen: false)..clearData();
 
@@ -17,6 +21,7 @@ Future showScanNPayBottomSheet(BuildContext context) async {
     child: ScanNPayPage(
       theme: theme,
       store: store,
+      upiDataModel: upiData,
       listOfPayUsing: listOfPayUsing,
     ),
   );
