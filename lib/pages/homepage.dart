7b017dc9a38b58d047427e8be545ef3f96f8fa83
Satyabrata Nayak <satyabratanayakofficial@gmail.com>
2025-01-05 02:03:30 +0530

    login feature integrated

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,12 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../core/flavors.dart';
+import '../features/auth/store/auth_store.dart';
 import '../theme/theme.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final authStore = Provider.of<AuthStore>(context);
     return Scaffold(
       backgroundColor: theme.colors.light,
       body: Center(
@@ -17,9 +20,9 @@ class HomeScreen extends StatelessWidget {
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
-                await testWriteData();
+                await authStore.googleLogout();
               },
-              child: const Text('Add Data'),
+              child: const Text('Logout'),
             )
           ],
         ),
