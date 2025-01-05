58c58fa0337f7988361fe57225e23b25dd74ec6d
Satyabrata Nayak <69607211+satyabratanayak@users.noreply.github.com>
2025-01-05 13:19:44 +0530

    login feature integrated (#62)

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,5 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import '../features/auth/ui/pages/login_page.dart';
 import '../pages/homepage.dart';
 import '../theme/theme.dart';
 import '../widgets/string_constants.dart';
@@ -29,9 +30,24 @@ class TacklePay extends StatelessWidget {
         theme: appTheme,
         debugShowCheckedModeBanner: false,
         home: _flavorBanner(
-          theme: Theme.of(context),
-          // App Entry
-          child: HomeScreen(),
-        ),
+            theme: Theme.of(context),
+            // App Entry
+            child: AuthWrapper()),
+      );
+}
+
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasData) {
+            return HomeScreen();
+          } else {
+            return const LoginScreen();
+          }
+        },
       );
 }
