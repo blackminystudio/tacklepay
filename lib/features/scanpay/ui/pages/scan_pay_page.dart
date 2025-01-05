16bf3371d71dc75ed0a66a7b2db387534fa8e51e
Satyabrata Nayak <69607211+satyabratanayak@users.noreply.github.com>
2025-01-05 13:19:23 +0530

    Feature/qr scan page (#61)
    * scan page build
    
    * QR scan implemented
    
    * Formatted
    
    * scan page build
    
    * QR scan implemented
    
    * Formatted
    
    * updated constants

--- lib/features/scanpay/ui/pages/scan_pay_page.dart
+++ lib/features/scanpay/ui/pages/scan_pay_page.dart
@@ -6,7 +6,9 @@ import '../../../../../widgets/cards/tag_card.dart';
 import '../../../../../widgets/cards/upi_card.dart';
 import '../../../../../widgets/cards/upi_info_card.dart';
 import '../../store/models/model_payusing.dart';
+import '../../store/models/model_upi_data.dart';
 import '../../store/scanpay_store.dart';
+import '../../utilities/constants.dart';
 
 class ScanNPayPage extends StatelessWidget {
   const ScanNPayPage({
@@ -14,11 +16,13 @@ class ScanNPayPage extends StatelessWidget {
     required this.theme,
     required this.store,
     required this.listOfPayUsing,
+    required this.upiDataModel,
   });
 
   final ThemeData theme;
   final ScanpayStore store;
   final List<PayUsingModel> listOfPayUsing;
+  final UpiDataModel upiDataModel;
 
   @override
   Widget build(BuildContext context) => Column(
@@ -37,7 +41,7 @@ class ScanNPayPage extends StatelessWidget {
                       const Icon(MinyIcons.navArrowLeft),
                       SizedBox(width: theme.sizing.width.s4),
                       Text(
-                        'Expense',
+                        expenseText,
                         style: theme.textStyle.titleRegular,
                       ),
                     ],
@@ -57,90 +61,90 @@ class ScanNPayPage extends StatelessWidget {
           ),
         ],
       );
-}
 
-Row _buildPayNowButtonBody(
-  List<PayUsingModel> listOfPayUsing,
-  ScanpayStore store,
-) =>
-    Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        Expanded(
-          child: PayUsingButton(listOfPayUsing: listOfPayUsing),
-        ),
-        ActionButton(
-          title: 'Pay Now',
-          onTap: () {
-            store.logAll();
-          },
-        )
-      ],
-    );
+  Row _buildPayNowButtonBody(
+    List<PayUsingModel> listOfPayUsing,
+    ScanpayStore store,
+  ) =>
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Expanded(
+            child: PayUsingButton(listOfPayUsing: listOfPayUsing),
+          ),
+          ActionButton(
+            title: payNowText,
+            onTap: () {
+              store.logAll();
+            },
+          )
+        ],
+      );
 
-StatefulBuilder _buildScanPayNowBody({
-  required ThemeData theme,
-  required ScanpayStore store,
-}) =>
-    StatefulBuilder(builder: (context, setState) {
-      final scrollController = ScrollController();
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        scrollController.animateTo(
-          scrollController.position.maxScrollExtent,
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.easeOut,
-        );
-      });
-      final upiDataModel = store.getUpiData();
-      return Expanded(
-        child: SingleChildScrollView(
-          controller: scrollController,
-          child: Column(
-            children: [
-              SizedBox(height: theme.sizing.height.s5),
-              UPICard(
-                payeeFirstName: upiDataModel.payeeFirstName,
-                payeeLastName: upiDataModel.payeeLastName,
-                payeeUpiId: upiDataModel.payeeUpiId,
-              ),
-              SizedBox(height: theme.sizing.height.s6),
-              UpiInfoCard(
-                onAmountChanged: store.setAmount,
-                onMessageChanged: store.setMessage,
-              ),
-              SizedBox(height: theme.sizing.height.s6),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Wrap(
-                  spacing: theme.spacing.width.s12,
-                  runSpacing: theme.spacing.width.s12,
-                  children: [
-                    ...store.tagList.map(
-                      (e) => TagCard(
-                        tagType: TagType.info,
-                        text: e,
-                        onDelete: () {
+  StatefulBuilder _buildScanPayNowBody({
+    required ThemeData theme,
+    required ScanpayStore store,
+  }) =>
+      StatefulBuilder(builder: (context, setState) {
+        final scrollController = ScrollController();
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          scrollController.animateTo(
+            scrollController.position.maxScrollExtent,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeOut,
+          );
+        });
+
+        return Expanded(
+          child: SingleChildScrollView(
+            controller: scrollController,
+            child: Column(
+              children: [
+                SizedBox(height: theme.sizing.height.s5),
+                UPICard(
+                  payeeFirstName: upiDataModel.payeeFirstName,
+                  payeeLastName: upiDataModel.payeeLastName,
+                  payeeUpiId: upiDataModel.payeeUpiId,
+                ),
+                SizedBox(height: theme.sizing.height.s6),
+                UpiInfoCard(
+                  onAmountChanged: store.setAmount,
+                  onMessageChanged: store.setMessage,
+                ),
+                SizedBox(height: theme.sizing.height.s6),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Wrap(
+                    spacing: theme.spacing.width.s12,
+                    runSpacing: theme.spacing.width.s12,
+                    children: [
+                      ...store.tagList.map(
+                        (e) => TagCard(
+                          tagType: TagType.info,
+                          text: e,
+                          onDelete: () {
+                            setState(() {
+                              store.deleteTag(e);
+                            });
+                          },
+                        ),
+                      ),
+                      TagCard(
+                        tagType: TagType.create,
+                        onTextSubmit: (value) {
                           setState(() {
-                            store.deleteTag(e);
+                            store.addTag(value);
                           });
                         },
                       ),
-                    ),
-                    TagCard(
-                      tagType: TagType.create,
-                      onTextSubmit: (value) {
-                        setState(() {
-                          store.addTag(value);
-                        });
-                      },
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
-            ],
+                SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
+              ],
+            ),
           ),
-        ),
-      );
-    });
+        );
+      });
+}
